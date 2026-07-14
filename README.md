f0703622df8113346183fd86119a90841808014f
Thiago V. <weegee521@gmail.com>
2026-07-13 22:16:42 -0300

    Segundo commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Antes de executar!
+
+``pip install -r requirements.txt``
+
+# P/ executar:
+*no caso de linux
+
+``sudo env "PATH=$PATH" flask run``
--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import ping3
 
 app = Flask(__name__)
 
-# Rota que entrega o seu HTML para o navegador
 @app.route("/")
 def index():
     return render_template("index.html")
